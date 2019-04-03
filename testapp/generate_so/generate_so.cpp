13d4f53e83e68e8c6209dd56c1da3f02a2a537fe
Juan Bujjamer <jubujjamer@gmail.com>
2019-04-03 18:37:05 -0300

    Added more functions to the cpp wrapper

--- testapp/generate_so/generate_so.cpp
+++ testapp/generate_so/generate_so.cpp
@@ -10,15 +10,7 @@ extern "C" {
 
   double VERSION = 1.00;
 
-  int camBinX = 1;
-  int camBinY = 1;
-
-
-  unsigned int roiStartX;
-  unsigned int roiStartY;
-  unsigned int roiSizeX;
-  unsigned int roiSizeY;
-
+  // Overscan is not yet implemented
   unsigned int overscanStartX;
   unsigned int overscanStartY;
   unsigned int overscanSizeX;
@@ -29,8 +21,6 @@ extern "C" {
   unsigned int effectiveSizeX;
   unsigned int effectiveSizeY;
 
-
-  unsigned int channels;
   // bool bNamedWindow = false;
   int oldMemLength = 0;
 
@@ -64,7 +54,7 @@ extern "C" {
     return pCamHandle;
     }
 
-  // set single frame mode
+  // Set single frame mode
   uint32_t set_stream_mode(qhyccd_handle *pCamHandle, int cam_mode){
     uint32_t ret_value;
     // cam_mode = 0; // 0 == single frame mode, 1 = video mode
@@ -72,31 +62,31 @@ extern "C" {
     return ret_value;
   }
 
-  // initialize camera
+  /* initialize camera
+  This function will initialize the camera hardware and other basic settings.
+  It should be call after SetQHYCCDStreamMode(). After calling this function,
+  the camera hardware will be ready to use. The SDK will initialize some basic
+  settings like the image bit depth, ROI, exposure time etc. These parameters
+  can also be changed later with the Parameter Command (SetQHYCCDParam()).*/
   uint32_t init_camera(qhyccd_handle *pCamHandle){
     uint32_t ret_value;
     ret_value = InitQHYCCD(pCamHandle);
     return ret_value;
   }
 
-  // set exposure time
+  // Setting exposure time
   uint32_t set_exposure(qhyccd_handle *pCamHandle, int exp_time){
     uint32_t ret_value;
     ret_value = SetQHYCCDParam(pCamHandle, CONTROL_EXPOSURE, exp_time);
     return ret_value;
   }
 
-
+  // Setting gain
+  uint32_t set_gain(qhyccd_handle *pCamHandle, int gain){
+    uint32_t ret_value;
     ret_value = IsQHYCCDControlAvailable(pCamHandle, CONTROL_GAIN);
     if (QHYCCD_SUCCESS == ret_value){
-        ret_value = SetQHYCCDParam(pCamHandle, CONTROL_GAIN, gain);
-        if (ret_value == QHYCCD_SUCCESS) {
-          printf("CONTROL_GAIN set to: %d, success\n", gain);}
-        else {
-          printf("CONTROL_GAIN failure, error: %d\n", ret_value);}
-    }
-    else{
-        printf("CONTROL_GAIN is not supported by this model.\n");}
+        ret_value = SetQHYCCDParam(pCamHandle, CONTROL_GAIN, gain);}
     return ret_value;
     }
 
@@ -148,7 +138,7 @@ extern "C" {
       return ret_value;
       }
 
-      /*Get CCD info_ Chip size, Pixel Size and Image Size*/
+      /*Get CCD info, Chip size, Pixel Size and Image Size*/
       uint32_t get_ccd_info(qhyccd_handle *pCamHandle){
         uint32_t ret_value;
         double chipWidthMM;
@@ -166,5 +156,100 @@ extern "C" {
         return ret_value;
     }
 
+    /*Dummy*/
+    uint32_t is_color(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      ret_value = IsQHYCCDControlAvailable(pCamHandle, CAM_COLOR);
+      if (ret_value == BAYER_GB || ret_value == BAYER_GR || ret_value == BAYER_BG || ret_value == BAYER_RG) {
+          SetQHYCCDDebayerOnOff(pCamHandle, true);
+          SetQHYCCDParam(pCamHandle, CONTROL_WBR, 20);
+          SetQHYCCDParam(pCamHandle, CONTROL_WBG, 20);
+          SetQHYCCDParam(pCamHandle, CONTROL_WBB, 20);}
+      return ret_value;}
+
+    /* Setting camera ROI.
+    These settings depend on the camera binning (see set_bin_mode())
+    In the example this parameters are set as
+
+    SetQHYCCDResolution(pCamHandle, 0, 0, maxImageSizeX, maxImageSizeY)
+    */
+    uint32_t set_resolution(qhyccd_handle *pCamHandle, unsigned int roiStartX, unsigned int roiStartY, unsigned int roiSizeX ,unsigned int roiSizeY){
+      uint32_t ret_value;
+      ret_value = SetQHYCCDResolution(pCamHandle, roiStartX, roiStartY, roiSizeX, roiSizeY);
+      return ret_value;}
+
+    /*Setting binning mode. In EZCap the available values are camBinX=camBinY=1 or 2*/
+    uint32_t set_bin_mode(qhyccd_handle *pCamHandle, int camBinX, int camBinY){
+      uint32_t ret_value;
+      ret_value = SetQHYCCDBinMode(pCamHandle, camBinX, camBinY);
+      return ret_value;}
+
+    /*Set bit depth of the images.
+    This funcion is not documented. I'll check some values to see watch it's behavior.
+    In the example bits = 16 is used. CHECK
+    */
+    uint32_t set_bits_mode(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      char bits = 16;
+      ret_value = IsQHYCCDControlAvailable(pCamHandle, CONTROL_TRANSFERBIT);
+      if (QHYCCD_SUCCESS == ret_value) {
+          ret_value = SetQHYCCDBitsMode(pCamHandle, bits);}
+      return ret_value;}
+
+    /*Start exposure*/
+    uint32_t expose_frame(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      ret_value = ExpQHYCCDSingleFrame(pCamHandle);
+      if (QHYCCD_ERROR != ret_value) {
+          if (QHYCCD_READ_DIRECTLY != ret_value) {
+              sleep(1);}
+          }
+      return ret_value;
+    }
+
+    /*Get memory length.
+    */
+    uint32_t get_memory_length(qhyccd_handle *pCamHandle){
+      uint32_t length = GetQHYCCDMemLength(pCamHandle);
+      return length;}
+
+    /*Get single frame.
+    I put length as an argument, but it should match with that of
+    GetQHYCCDMemLength().
+    I don't know if ROI sizes should match with those from set_resolution.
+    I don't know why pImgData are defined as unsigned chars, as they should be
+    16 bits arrays. One possibility is that the lenght doubles the real length
+    of the array. CHECK.
+    */
+    unsigned char * get_single_frame(qhyccd_handle *pCamHandle, unsigned int roiSizeX, unsigned int roiSizeY, unsigned int bpp, unsigned int channels, uint32_t length){
+      uint32_t ret_value;
+      unsigned char *pImgData = 0;
+      pImgData = new unsigned char[length];
+      ret_value = GetQHYCCDSingleFrame(pCamHandle, &roiSizeX, &roiSizeY, &bpp, &channels, pImgData);
+      return pImgData;}
+
+    /*Canceling exposure*/
+    uint32_t cancel_exposure(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      ret_value = CancelQHYCCDExposingAndReadout(pCamHandle);
+      return ret_value;}
+
+    /*Closing the camera port*/
+    uint32_t close_camera(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      ret_value = CloseQHYCCD(pCamHandle);
+      return ret_value;}
+
+    /*Release the SDK resources*/
+    uint32_t release(void){
+      uint32_t ret_value;
+      ret_value = ReleaseQHYCCDResource();
+      return ret_value;}
+
+    /*Dummy*/
+    uint32_t dummy(qhyccd_handle *pCamHandle){
+      uint32_t ret_value;
+      // ret_value = Dummy(pCamHandle);
+      return ret_value;}
 
 }
